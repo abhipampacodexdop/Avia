e5d8c56558fcb697c0ba77adf589c8bde396f595
abhipampacodexdop <140977883+abhipampacodexdop@users.noreply.github.com>
2023-11-07 01:24:27 +0530

    Update config.py
    Ol

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamAviax/AviaxMusic",
+    "https://github.com/abhipampacodexdop/Avia",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
